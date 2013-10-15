241299a3d02327618ecf28d6f0a3f32e920229e9
Brian Clymer <crdnilfan@gmail.com>
2013-10-14 21:03:03 -0500

    Some perf, don't move what doesn't need to move.

--- static/js/app.js
+++ static/js/app.js
@@ -65,37 +65,49 @@
 	});
 
 	enquire.register("(min-width: 1600px)", {
-    	match : function() {
-    		AboutMe.responsiveState = 1;
-    		moveListToElement(AboutMe.events.stack, $('#stack-cards'));
-    		moveListToElement(AboutMe.events.github, $('#github-cards'));
-    		moveListToElement(AboutMe.events.twitter, $('#twitter-cards'));
-    		moveListToElement(AboutMe.events.facebook, $('#facebook-cards'));
-    	}
+		match : function() {
+			if (AboutMe.responsiveState >= 3) {
+				moveListToElement(AboutMe.events.stack, $('#stack-cards'));
+				moveListToElement(AboutMe.events.github, $('#github-cards'));
+			}
+			if (AboutMe.responsiveState >= 2) {
+				moveListToElement(AboutMe.events.twitter, $('#twitter-cards'));
+				moveListToElement(AboutMe.events.facebook, $('#facebook-cards'));
+			}
+			AboutMe.responsiveState = 1;
+		}
 	});
 
 	enquire.register("(min-width: 1200px) and (max-width: 1599px)", {
-    	match : function() {
-    		AboutMe.responsiveState = 2;
-			moveListToElement(AboutMe.events.social, $('#social-cards'));
-    		moveListToElement(AboutMe.events.stack, $('#stack-cards'));
-    		moveListToElement(AboutMe.events.github, $('#github-cards'));
-    	}
+		match : function() {
+			if (AboutMe.responsiveState == 1 || AboutMe.responsiveState == 4) {
+				moveListToElement(AboutMe.events.social, $('#social-cards'));
+			}
+			if (AboutMe.responsiveState >= 3) {
+				moveListToElement(AboutMe.events.stack, $('#stack-cards'));
+				moveListToElement(AboutMe.events.github, $('#github-cards'));
+			}
+			AboutMe.responsiveState = 2;
+		}
 	});
 
 	enquire.register("(min-width: 800px) and (max-width: 1199px)", {
-    	match : function() {
-    		AboutMe.responsiveState = 3;
-			moveListToElement(AboutMe.events.social, $('#social-cards'));
-    		moveListToElement(AboutMe.events.tech, $('#tech-cards'));
-    	}
+		match : function() {
+			if (AboutMe.responsiveState == 1 || AboutMe.responsiveState == 4) {
+				moveListToElement(AboutMe.events.social, $('#social-cards'));
+			}
+			if (AboutMe.responsiveState <= 2 || AboutMe.responsiveState == 4) {
+				moveListToElement(AboutMe.events.tech, $('#tech-cards'));
+			}
+			AboutMe.responsiveState = 3;
+		}
 	});
 
 	enquire.register("(max-width: 799px)", {
-    	match : function() {
-    		AboutMe.responsiveState = 4;
-    		moveListToElement(AboutMe.events.all, $('#all-cards'));
-    	}
+		match : function() {
+			moveListToElement(AboutMe.events.all, $('#all-cards'));
+			AboutMe.responsiveState = 4;
+		}
 	});
 
 	function moveListToElement(list, element) {
